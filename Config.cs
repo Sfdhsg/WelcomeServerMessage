7894407e64d6115474b0f782c7333e6eae00c405
sanya_new_coder <101363348+Sfdhsg@users.noreply.github.com>
2024-11-20 21:03:24 +0300

    versions 1.0

---
+++ Config.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.API.Interfaces;
+
+
+namespace Broadcasts
+{
+    public class Config : IConfig
+    {
+         public bool IsEnabled { get; set; } = true;
+
+         public bool Debug { get; set; } = false;
+         public ushort Duration { get; set; } = 10;
+        public string Text { get; set; } = "<color=green>welcome to the server</color>";
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.API.Features;
+
+using Exiled.Events.EventArgs.Player;
+
+namespace Broadcasts
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Author => "Fatal Error 404";
+        public override string Name => "WelcomeServerMesssage";
+        public override string Prefix => "WelcomeServerMesssage";
+        public override Version Version => base.Version;
+        public override void OnEnabled()
+        {
+            Exiled.Events.Handlers.Player.Verified += OnVerified;
+            base.OnEnabled();
+        }
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Player.Verified -= OnVerified;
+            base.OnDisabled();
+        }
+        private void OnVerified(VerifiedEventArgs ev)
+        {
+            if (ev.Player == null && ev.Player.IsNPC && ev.Player.IsHost)
+                return;
+
+            ev.Player.Broadcast(Config.Duration, Config.Text);
+        }
+    }
+}
