4e6749c7a920eba377a3a6b70745ea371cfe9c73
sanya_new_coder <101363348+Fatal-Error-404-developer@users.noreply.github.com>
2024-11-25 19:21:26 +0300

    version 1.5.0

--- Config.cs
+++ Config.cs
@@ -5,16 +5,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Exiled.API.Interfaces;
+using Exiled.API.Features;
+using System.ComponentModel;
 
 
 namespace Broadcasts
 {
     public class Config : IConfig
     {
-         public bool IsEnabled { get; set; } = true;
+        [Description("Включен ли плагин")]
+        public bool IsEnabled { get; set; } = true;
 
-         public bool Debug { get; set; } = false;
-         public ushort Duration { get; set; } = 10;
+        [Description("Включен ли дебагмод")]
+        public bool Debug { get; set; } = false;
+
+        [Description("время бродкаста")]
+        public ushort Duration { get; set; } = 10;
+
+        [Description("текст бродкаста")]
         public string Text { get; set; } = "<color=green>welcome to the server</color>";
+
+        [Description("текст броадкаста о побеге человека")]
+        public string TextEscape { get; set; } = "You Escape";
+
+        [Description("текст броадкаста о Смерти")]
+        public string TextDie { get; set; } = "Ypu die";
+
+        [Description("текст броадкаста о Смерти")]
+        public string TextEndRound { get; set; } = "Round End";
     }
 }
--- Plugin.cs
+++ Plugin.cs
@@ -4,8 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Exiled.API.Features;
-
+using Exiled.Events.Commands.PluginManager;
 using Exiled.Events.EventArgs.Player;
+using Exiled.Events.EventArgs.Server;
+using Exiled.API.Interfaces;
+
+using PluginAPI.Events;
 
 namespace Broadcasts
 {
@@ -19,10 +23,28 @@ namespace Broadcasts
         {
             Exiled.Events.Handlers.Player.Verified += OnVerified;
             base.OnEnabled();
+
+            Exiled.Events.Handlers.Player.Escaping += OnEscape;
+            base.OnEnabled();
+
+            Exiled.Events.Handlers.Player.Died += OnPlayerDead;
+            base.OnEnabled();
+
+            Exiled.Events.Handlers.Server.RoundEnded += OnRoundEnd;
+            base.OnEnabled();
         }
         public override void OnDisabled()
         {
             Exiled.Events.Handlers.Player.Verified -= OnVerified;
+            base.OnDisabled();
+
+            Exiled.Events.Handlers.Player.Escaping += OnEscape;
+            base.OnDisabled();
+
+            Exiled.Events.Handlers.Player.Died += OnPlayerDead;
+            base.OnDisabled();
+
+            Exiled.Events.Handlers.Server.RoundEnded += OnRoundEnd;
             base.OnDisabled();
         }
         private void OnVerified(VerifiedEventArgs ev)
@@ -32,5 +54,28 @@ namespace Broadcasts
 
             ev.Player.Broadcast(Config.Duration, Config.Text);
         }
+        private void OnEscape(EscapingEventArgs ev)
+        {
+            if (ev.Player == null && ev.Player.IsNPC && ev.Player.IsHost)
+                return;
+
+            ev.Player.Broadcast(Config.Duration, Config.TextEscape);
+        }
+        private void OnPlayerDead(DiedEventArgs ev)
+        {
+
+            if (ev.Player == null)
+                return;
+
+            ev.Player.Broadcast(Config.Duration, Config.TextDie);
+        }
+
+        private void OnRoundEnd(RoundEndedEventArgs ev)
+        {
+            Map.Broadcast(Config.Duration, Config.TextEndRound);
+        }
+
+
+
     }
 }
